9853902e22ea85023d95e4d9a9720087aa6b00c6
Michał Weskida <michal.weskida@gmail.com>
2022-10-04 19:04:10 +0200

    fix: updated package.lock, added dependencies, added commit step to publish workflow

--- libraries/library-2/index.js
+++ libraries/library-2/index.js
@@ -1,7 +1,6 @@
-// TODO uncomment this after the initial release
-// import libraryHelperVariable from '@mhal007/monorepo-npm-library-helper'
-//
-// console.log('library-helper variable value is ' + libraryHelperVariable)
+import libraryHelperVariable from '@mhal007/monorepo-npm-library-helper'
+
+console.log('library-helper variable value is ' + libraryHelperVariable)
 
 console.log('Hello World from library-2');
 
